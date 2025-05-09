6c0a7b04a5ea943879f07a726e7a381963a30d46
Tiluiq <63489860+Tiluiq@users.noreply.github.com>
2025-05-09 09:35:00 +0900

    削除漏れ

--- Assets/Main/Domain/AdvanceScenario/Test/AdvanceScenarioUseCaseTest.cs
+++ Assets/Main/Domain/AdvanceScenario/Test/AdvanceScenarioUseCaseTest.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using Entity.Scenario;
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using Domain.Interface.Presenter;
 using System.Threading.Tasks;
 
 namespace Domain.AdvanceScenario.Test
@@ -11,35 +10,6 @@ namespace Domain.AdvanceScenario.Test
 {
     public class AdvanceScenarioUseCaseTest
     {
-
-        class MockTextPresenter : ITextWindowPresenter
-        {
-            public string LastName { get; private set; }
-            public string LastText { get; private set; }
-
-            public bool IsShowTextWindowCalled { get; private set; }
-            public bool IsHideTextWindowCalled { get; private set; }
-
-            public UniTask ShowTextWindowAsync(bool showName, CancellationToken cancellationToken = default)
-            {
-                IsShowTextWindowCalled = true;
-                return UniTask.CompletedTask;
-            }
-
-            public UniTask HideTextWindowAsync(CancellationToken cancellationToken = default)
-            {
-                IsHideTextWindowCalled = true;
-                return UniTask.CompletedTask;
-            }
-
-            public UniTask ShowTextAsync(string name, string text, CancellationToken cancellationToken = default)
-            {
-                LastName = name;
-                LastText = text;
-                return UniTask.CompletedTask;
-            }
-        }
-
         private MockScenarioProvider mockScenarioProvider;
         private MockTextPresenter mockTextPresenter;
         private MockAnimationPresenter mockAnimationPresenter;
