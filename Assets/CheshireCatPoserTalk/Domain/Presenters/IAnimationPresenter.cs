cd68dc5f2faf694411f9312711ac96ee98118bdf
Tiluiq <63489860+Tiluiq@users.noreply.github.com>
2025-05-14 13:25:05 +0900

    Presenterの名前空間修正

--- Assets/CheshireCatPoserTalk/Domain/Presenters/IAnimationPresenter.cs
+++ Assets/CheshireCatPoserTalk/Domain/Presenters/IAnimationPresenter.cs
@@ -1,7 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CheshireCatPoserTalk.Domain.Presenter
+namespace CheshireCatPoserTalk.Domain.Presenters
 {
     public interface IAnimationPresenter
     {
--- Assets/CheshireCatPoserTalk/Domain/UseCases/AdvanceScenario/AdvanceScenarioUseCase.cs
+++ Assets/CheshireCatPoserTalk/Domain/UseCases/AdvanceScenario/AdvanceScenarioUseCase.cs
@@ -1,6 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
-using CheshireCatPoserTalk.Domain.Presenter;
+using CheshireCatPoserTalk.Domain.Presenters;
 using CheshireCatPoserTalk.Domain.Services;
 using CheshireCatPoserTalk.Entity.Scenario;
 
